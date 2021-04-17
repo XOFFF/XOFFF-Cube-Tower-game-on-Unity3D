a266636bc21e9e988ec2532e2992abeedc08ad95
Marko Andrushchenko <markello123456789101@gmail.com>
2021-04-17 13:22:45 +0200

    Add the first project

---
+++ First Unity Project/Assets/Scripts/Camera/RotateCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float speed = 5f;
+    private Transform rotator;
+
+    private void Start()
+    {
+        rotator = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        rotator.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
